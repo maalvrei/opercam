3339d796fe0a467a5cb96167c3fa38632c41f19d
maalvrei <miguemorisco@gmail.com>
2024-06-09 21:32:08 +0200

    modificando home boton back

--- opercam/src/main/java/com/app/opercam/service/controller/PuestoController.java
+++ opercam/src/main/java/com/app/opercam/service/controller/PuestoController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.app.opercam.model.entity.LineaProduccion;
 import com.app.opercam.model.entity.Rechazo;
@@ -64,5 +66,14 @@ public class PuestoController {
 		model.addAttribute("rechazos",rechazos);
 		model.addAttribute("puesto",5);
 		return "puesto";
+	}
+	
+	@PostMapping("/puesto/filtro")
+	public String puestoFiltrado(@RequestParam("producto") String producto, Model model) {
+	    List<LineaProduccion> puestoFiltrado = lineaProduccionService.findByProducto(producto);
+	    List<Rechazo> rechazos = rechazoService.rechazoPorPuesto(puestoFiltrado);
+	    model.addAttribute("rechazos", rechazos);
+	    model.addAttribute("puesto", "FILTRADO POR " + producto);
+	    return "puesto";
 	}
 }
